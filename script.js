85f0acaea224938d2b2a395774db0993fba409f2
Aditya Patole <patolekrrish2006@gmail.com>
2026-08-28 22:28:42 +0530

    Update WEEKLY_SUMMARY_API URL in script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // Replace these with your Production Webhook URLs
 const ADD_EXPENSE_API = "https://krrish2006.app.n8n.cloud/webhook-test/0351975d-ca91-4cf2-982e-ab9213816439";
-const WEEKLY_SUMMARY_API = "https://krrish2006.app.n8n.cloud/webhook-test/weekly-summary";
+const WEEKLY_SUMMARY_API = "https://aladitya2006.app.n8n.cloud/webhook-test/weekly-summary";
 
 // Temporary User ID
 // Later we'll replace this with login/localStorage
